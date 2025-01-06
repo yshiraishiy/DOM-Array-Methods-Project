a4f10e8460fb6cd225fecc0eb77dda9b5ca15779
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-06 17:00:58 +0900

    ✨ランダムなユーザーを作成

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+
+// フェッチでランダムなユーザーを取得し金額を取得
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  }
+
+  addData(newUser)
+}
+
+// 新しいオブジェクトを配列に追加
+function addData(obj) {
+  data.push(obj)
+  console.log(data)
+}
